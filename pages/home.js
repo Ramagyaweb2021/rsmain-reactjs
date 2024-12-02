c11fce9212b7f0422a538c55776ae476a556e09a
@websitedeveloper1 <websitedeveloper@ramagyaschool.com>
2024-12-02 11:59:15 +0530

    -changes in homep

--- pages/home.js
+++ pages/home.js
@@ -144,7 +144,7 @@ export default function Home() {
 
       {!isMobile ? (
         <ReactFullpage
-          debug={true}
+          debug={false}
           anchors={[
             "slider", "branch", "explore", "experiment", "innovate",
             "evolve", "lead", "difference", "gallery", "awards", 
@@ -155,7 +155,8 @@ export default function Home() {
           responsiveWidth={1000}
           onLeave={onLeave}
           afterLoad={afterLoad}
-          scrollBar={false}  // Make sure to disable custom scrollbar
+          scrollBar={true}  // Make sure to disable custom scrollbar
+          // autoScrolling: {true}
           scrollingSpeed={700}  // Adjust scrolling speed to avoid skipping sections
           //scrollingSpeed={900}  // Adjust scrolling speed to avoid skipping sections
           fitToSection={true}  // Ensure the scroll stops on each section
