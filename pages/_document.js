ed819df62a9fd5b9f7527cf9379684b000d8a937
@websitedeveloper1 <websitedeveloper@ramagyaschool.com>
2025-02-03 16:46:55 +0530

    changes in components and css, images

--- pages/_document.js
+++ pages/_document.js
@@ -25,8 +25,8 @@ export default function Document() {
         {/* <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link> */}
-        <link rel="preconnect" href="https://forms.edunexttechnologies.com/forms/ramagya/main-branch" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
+        {/* <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link> */}
       <Head/> 
       <body>
         <Main />
